f06ca830b14f6bd3049ae4b25556e4fdd52ccd67
Dinesh Kumar <dinesh78088@gmail.com>
2023-01-04 16:24:58 +0530

    UI updates

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 let featchURL = "https://api.openweathermap.org/data/2.5/weather?q="
-let cityName = null;
+let cityName = '';
 let apiKey = "05017894b8b0ac83af72659f3dc9d03c"
 
+// weatherData('new delhi')
 
-document.querySelector('.serach-box').addEventListener('keyup', event => {
+document.querySelector('.serach').addEventListener('keyup', event => {
     let { keyCode, target: input } = event
 
     if (keyCode === 13) {
         console.log(input.value)
         cityName = input.value
         weatherData(cityName)
+
+        document.querySelector('.serach').value = ''
     }
-
-    function weatherData(CityName){
-        console.log('weather data function')
-        fetch(`${featchURL}${CityName}&appid=${apiKey}`)
-        .then(res => res.json())
-        .then(cityData =>{
-            console.log(cityData)
-            document.querySelector('.city').innerHTML = `${cityData.name}, ${cityData.sys.country}`
-            document.querySelector('.temp').innerHTML = `${Math.floor(cityData.main.temp - 273.15)} *c`
-            document.querySelector('.weather').innerHTML = `${cityData.weather[0].main}`
-            document.querySelector('.hi-low').innerHTML = `${Math.floor(cityData.main.temp_min - 273.15)} - ${Math.floor(cityData.main.temp_max - 273.15)}`
-        })  
-        .catch(err => alert('City not found'))      
-
-
-    }
-
 
 })
+
+function weatherData(CityName){
+    console.log('weather data function')
+    fetch(`${featchURL}${CityName}&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(cityData =>{
+        console.log(cityData)
+        document.querySelector('.city').innerHTML = `${cityData.name}, ${cityData.sys.country}`
+        document.querySelector('.temperature').innerHTML = `${Math.floor(cityData.main.temp - 273.15)}°c`
+        document.querySelector('.weather').innerHTML = `${cityData.weather[0].main}`
+        document.querySelector('.min-max').innerHTML = `Min ${Math.floor(cityData.main.temp_min - 273.15)}°c / Max ${Math.floor(cityData.main.temp_max - 273.15)}°c`
+    })  
+    .catch(err => alert('City not found'))      
+}
+
+weatherData('New Delhi')
